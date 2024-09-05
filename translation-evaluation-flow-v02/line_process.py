adc3de2ece481c472b6df351fe7ab6c471161604
Your Name <you@example.com>
2024-09-05 11:10:29 +0800

    ran the smoke test of standard-flow-v01 and evaluation-flow-v02

--- translation-evaluation-flow-v02/line_process.py
+++ translation-evaluation-flow-v02/line_process.py
@@ -1,7 +1,8 @@
 # ---------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
-
+import os
+from dotenv import load_dotenv
 from promptflow.core import tool
 from promptflow.core import Prompty
 
@@ -16,17 +17,28 @@ def line_process(original_english_context_file_path: str, current_english_chart_
     :param translated_chinese_text: the translated chinese text of the current context file path.
     """
 
-    # Add your line processing logic here
-    elegance_prompty = Prompty.load(source="/Users/huqianghui/Downloads/books-translations/translation-evaluation-flow-v01/prompty/elegance.prompty")
-    expressiveness_prompty = Prompty.load(source="/Users/huqianghui/Downloads/books-translations/translation-evaluation-flow-v01/prompty/expressiveness.prompty")
-    faithfulness_prompty = Prompty.load(source="/Users/huqianghui/Downloads/books-translations/translation-evaluation-flow-v01/prompty/faithfulness.prompty")
+    if "OPENAI_API_KEY" not in os.environ and "AZURE_OPENAI_API_KEY" not in os.environ:
+        # load environment variables from .env file
+        load_dotenv()
 
+    prjroot = os.path.normpath(os.path.join(os.path.abspath(__file__),"..",".."))
+    # Add your line processing logic here
+    elegance_prompty = Prompty.load(source=os.path.normpath(os.path.join(os.path.abspath(__file__),"..","prompty","elegance.prompty")))
+    expressiveness_prompty = Prompty.load(source=os.path.normpath(os.path.join(os.path.abspath(__file__),"..","prompty","expressiveness.prompty")))
+    faithfulness_prompty = Prompty.load(source=os.path.normpath(os.path.join(os.path.abspath(__file__),"..","prompty","faithfulness.prompty")))
+    
+    #/Users/huqianghui/Downloads/books-translations/books/TheInnovatorsDilemmaWhenNewTechnoloClaytonMChristensen-chart1.md
+    original_english_context_file_path = os.path.join(prjroot, "books",original_english_context_file_path)
     with open(original_english_context_file_path, 'r', encoding='utf-8') as original_english_context_file:
         original_english_context = original_english_context_file.read()
 
+    #/Users/huqianghui/Downloads/books-translations/book-chunks/TheInnovatorsDilemmaWhenNewTechnoloClaytonMChristensen-chart1-00.md
+    current_english_chart_file_path = os.path.join(prjroot, "book-chunks",current_english_chart_file_path)
     with open(current_english_chart_file_path, 'r', encoding='utf-8') as current_english_chart_file:
         current_english_chart = current_english_chart_file.read()
 
+    #/Users/huqianghui/Downloads/books-translations/translated-book-chunks/final-publishing-version-book-chunks/TheInnovatorsDilemmaWhenNewTechnoloClaytonMChristensen-chart1-00.md
+    translated_chinese_text_file_path = os.path.join(prjroot, "translated-book-chunks", "final-publishing-version-book-chunks", translated_chinese_text_file_path)
     with open(translated_chinese_text_file_path, 'r', encoding='utf-8') as translated_chinese_text_file:
         translated_chinese_text = translated_chinese_text_file.read()
 
--- translation-standard-flow-v01/python_node_save_translation_result.py
+++ translation-standard-flow-v01/python_node_save_translation_result.py
@@ -1,27 +1,32 @@
+import os
+from dotenv import load_dotenv
+from promptflow.core import tool
 
-from promptflow import tool
 
-
-translated_path = '/Users/huqianghui/Downloads/books-translations/translated-book-chunks'
+# translated_path = '/Users/huqianghui/Downloads/books-translations/translated-book-chunks'
+translated_path = os.path.normpath(os.path.join(os.path.abspath(__file__),"..","..","translated-book-chunks"))
 
 # The inputs section will change based on the arguments of the tool function, after you save the code
 # Adding type to arguments and return value will help the system show the types properly
 # Please update the function name/signature per need
+
 @tool
 def my_python_tool(file_name:str,
                    first_translated_text: str=None,
                    second_translated_text: str=None,
                    final_translated_text: str=None) -> None:
     if(final_translated_text is not None):
-        with open(translated_path + '/final-publishing-version-book-chunks/' + file_name, 'w',encoding='utf-8') as f:
+        with open(os.path.join(translated_path, 'final-publishing-version-book-chunks', file_name), 'w',encoding='utf-8') as f:
             f.write(second_translated_text)
     
     if(second_translated_text is not None):
-        with open(translated_path + '/second-Proofreading-version-book-churnks/' + file_name, 'w',encoding='utf-8') as f:
+        with open(os.path.join(translated_path, 'second-Proofreading-version-book-churnks', file_name), 'w',encoding='utf-8') as f:
             f.write(second_translated_text)
     
     if(first_translated_text is not None):
-        with open(translated_path + '/first-version-translated-book-chunks/' + file_name, 'w',encoding='utf-8') as f:
+        with open(os.path.join(translated_path, 'first-version-translated-book-chunks', file_name), 'w',encoding='utf-8') as f:
             f.write(first_translated_text)
 
     return None
+
+# print(os.path.normpath(os.path.join(os.path.abspath(__file__),"..","..","translated-book-chunks")))
